9b7a1586735f99e3e020bc94fc73943aa5afbd6e
kia <akia2223@gmail.com>
2021-01-04 21:32:17 +0330

    progress bar

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -1,9 +1,11 @@
-library("shiny")
-library("shinyjs")
-library("shinydashboard")
 library("dplyr")
 library("plotly")
 library("forecast")
+
+library("shiny")
+library("shinyjs")
+# library("shinydashboard")
+# library("shinycssloaders")
 
 #----
 # initialize data frame
@@ -79,6 +81,7 @@ ui <- navbarPage("Stock Price Prediction",
             mainPanel(
                 fluidRow(
                     plotlyOutput("symbol_plot"),
+                    
                     plotlyOutput("predict_plot"),
                 ),
                 fluidRow(
@@ -93,6 +96,7 @@ ui <- navbarPage("Stock Price Prediction",
         ) 
     )
 )
+
 
 server <- function(input, output, session) {
     stock_df <- reactive({
@@ -137,12 +141,16 @@ server <- function(input, output, session) {
         switch(
             input$model_in, 
             "gbm" = "gbm$",
-            "ets" = (do_ets(stock.symbol = input$symbol_in, trades = stock_df(),
-                        window.size = input$windowSize)),
+            "ets" = (
+                do_ets(stock.symbol = input$symbol_in, 
+                    trades = stock_df(), window.size = input$windowSize)
+            ),
             "dl" = "dl$",
             "rf" = "rf$",
-            "reg" = (do_reg(stock.symbol = input$symbol_in, trades = stock_df(),
-                        window.size = input$windowSize)),
+            "reg" = (
+                do_reg(stock.symbol = input$symbol_in, 
+                    trades = stock_df(), window.size = input$windowSize)
+            ),
             "multireg" = "multi$",
             "dlgrid" = "gird$"
         )
@@ -208,113 +216,125 @@ server <- function(input, output, session) {
     
 }
 
+
 shinyApp(ui, server)
+
 
 #----
 # exponential smoothing
 do_ets <- function(stock.symbol, trades, window.size) {
-    model = "MNN"
-    rows.number <- length(model) * length(stock.symbol) 
-    
-    pred.cols <- c("symbol", "date", "actual_final_price", "change", "model", 
-                   "predicted_price", "ME", "MAE", "RMSE", "MPE", "MAPE", "MASE")
-    pred.df <- data.frame(matrix(nrow=0, ncol=length(pred.cols)))
-    names(pred.df) <- pred.cols
-    
-    stock.df <- trades %>% filter(symbol == stock.symbol) %>% select(
-        "date", "final_price", "change")
-    
-    stock.ts <- ts(stock.df$final_price, frequency = 1)
-    
-    len <- length(stock.ts)
-    
-    valid.size <- 1
-    train.size <- window.size
-    
-    #rep(x, y): replicate x, y times
-    ets.me.list <- rep(0, len - train.size - valid.size + 1)
-    ets.mae.list <- rep(0, len - train.size - valid.size + 1) 
-    ets.rmse.list <- rep(0, len - train.size - valid.size + 1)
-    ets.mpe.list <- rep(0, len - train.size - valid.size + 1)
-    ets.mape.list <- rep(0, len - train.size - valid.size + 1)
-    ets.mase.list <- rep(0, len - train.size - valid.size + 1)
-    
-    for(j in 1 : (len - train.size - valid.size)) {
-        day.index <- j + train.size + valid.size
+    withProgress(message = 'Calculating', value = 0, {
+        model = "MNN"
+        rows.number <- length(model) * length(stock.symbol) 
         
-        train.ts <- window(stock.ts, start = j , end = j + train.size)
-        
-        valid.ts <- window(stock.ts, start = j + train.size + 1, 
-                           end = day.index)
-        
-        ets.fit <- ets(
-            train.ts, model = model
+        pred.cols <- c(
+            "symbol", "date", "actual_final_price", "change", "model", 
+            "predicted_price", "ME", "MAE", "RMSE", "MPE", "MAPE", "MASE"
         )
+        pred.df <- data.frame(matrix(nrow=0, ncol=length(pred.cols)))
+        names(pred.df) <- pred.cols
         
-        ets.pred <- forecast(ets.fit, h = valid.size)
+        stock.df <- trades %>% filter(symbol == stock.symbol) %>% select(
+            "date", "final_price", "change")
         
-        ets.me.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"ME"]
-        ets.mae.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"MAE"]
-        ets.rmse.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"RMSE"]
-        ets.mpe.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"MPE"]
-        ets.mape.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"MAPE"]
-        ets.mase.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"MASE"]
+        stock.ts <- ts(stock.df$final_price, frequency = 1)
         
-        if(j%%40==0){print(j)}
+        len <- length(stock.ts)
         
-        temp.pred.df <- data.frame(matrix(
-            nrow=1, ncol=length(pred.cols)))
+        valid.size <- 1
+        train.size <- window.size
         
-        tdate <- stock.df[day.index,]$date
-        actual.price <- valid.ts[[1]]
-        pred.price <- ets.pred$mean[[1]]
+        #rep(x, y): replicate x, y times
+        ets.me.list <- rep(0, len - train.size - valid.size + 1)
+        ets.mae.list <- rep(0, len - train.size - valid.size + 1) 
+        ets.rmse.list <- rep(0, len - train.size - valid.size + 1)
+        ets.mpe.list <- rep(0, len - train.size - valid.size + 1)
+        ets.mape.list <- rep(0, len - train.size - valid.size + 1)
+        ets.mase.list <- rep(0, len - train.size - valid.size + 1)
         
-        change <- stock.df[day.index,]$change
-        
-        temp.pred.df <- data.frame(
-            stock.symbol, tdate, actual.price, change, model, pred.price, 
-            ets.me.list[[j]], ets.mae.list[[j]], ets.rmse.list[[j]],
-            ets.mpe.list[[j]], ets.mape.list[[j]], ets.mase.list[[j]]
-        )
-        
-        colnames(temp.pred.df) <- pred.cols
-        
-        pred.df <- rbind(pred.df, temp.pred.df)
-    }
-    
-    p <- list("model" = model, "sym" = stock.symbol, 
-              "mape" = mean(ets.mape.list), "rmse" = mean(ets.rmse.list))
-    print(p)
-    
-    pred.df[,"date"] <- as.character(pred.df[,"date"])
-    
-    profit <- sign(
-        (pred.df$predicted_price - shift_vector(pred.df$actual_final_price, 1))* 
-            pred.df$change
-    )
-    
-    result <- list(
-        "df" = pred.df,
-        
-        "error_parameters" = data.frame("mape" = p$mape, "rmse" = p$rmse),
-        
-        "predictions" = pred.df[,c("date", "actual_final_price", 
-                                   "predicted_price")],
-        
-        "residuals" = pred.df[,c("date", "ME")],
-        
-        "profits" = data.frame(
-            "profit" = c("Wrong", "Right"), 
-            "color" = c("red", "green"),
-            "sum" = c(
-                abs(sum(profit[profit == -1])),
-                sum(profit[profit != -1])
+        for(j in 1 : (len - train.size - valid.size)) {
+            day.index <- j + train.size + valid.size
+            
+            train.ts <- window(stock.ts, start = j , end = j + train.size)
+            
+            valid.ts <- window(stock.ts, start = j + train.size + 1, 
+                               end = day.index)
+            
+            ets.fit <- ets(
+                train.ts, model = model
             )
-        ),
+            
+            ets.pred <- forecast(ets.fit, h = valid.size)
+            
+            ets.me.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"ME"]
+            ets.mae.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"MAE"]
+            ets.rmse.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"RMSE"]
+            ets.mpe.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"MPE"]
+            ets.mape.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"MAPE"]
+            ets.mase.list[[j]] <- accuracy(ets.pred, valid.ts)[2,"MASE"]
+            
+            if(j%%40==0){print(j)}
+            
+            incProgress(
+                1/(len - train.size - valid.size), 
+                detail = paste(j, " th day")
+            )
+            
+            temp.pred.df <- data.frame(matrix(
+                nrow=1, ncol=length(pred.cols)))
+            
+            tdate <- stock.df[day.index,]$date
+            actual.price <- valid.ts[[1]]
+            pred.price <- ets.pred$mean[[1]]
+            
+            change <- stock.df[day.index,]$change
+            
+            temp.pred.df <- data.frame(
+                stock.symbol, tdate, actual.price, change, model, pred.price, 
+                ets.me.list[[j]], ets.mae.list[[j]], ets.rmse.list[[j]],
+                ets.mpe.list[[j]], ets.mape.list[[j]], ets.mase.list[[j]]
+            )
+            
+            colnames(temp.pred.df) <- pred.cols
+            
+            pred.df <- rbind(pred.df, temp.pred.df)
+        }
         
-        "profit_percent" = round(sum(profit[profit != -1]/length(profit)), 4)
+        p <- list("model" = model, "sym" = stock.symbol, 
+                  "mape" = mean(ets.mape.list), "rmse" = mean(ets.rmse.list))
+        print(p)
         
-    )
+        pred.df[,"date"] <- as.character(pred.df[,"date"])
+        
+        profit <- sign(
+            ( pred.df$predicted_price - 
+                shift_vector(pred.df$actual_final_price, 1)) * pred.df$change
+        )
+        
+        result <- list(
+            "df" = pred.df,
+            
+            "error_parameters" = data.frame("mape" = p$mape, "rmse" = p$rmse),
+            
+            "predictions" = pred.df[,c("date", "actual_final_price", 
+                                       "predicted_price")],
+            
+            "residuals" = pred.df[,c("date", "ME")],
+            
+            "profits" = data.frame(
+                "profit" = c("Wrong", "Right"), 
+                "color" = c("red", "green"),
+                "sum" = c(
+                    abs(sum(profit[profit == -1])),
+                    sum(profit[profit != -1])
+                )
+            ),
+            
+            "profit_percent" = round(
+                sum(profit[profit != -1]/length(profit)), 4
+            )
+        )
+    })
     print("__________Finished__________")
     
     return (result)
@@ -323,106 +343,118 @@ do_ets <- function(stock.symbol, trades, window.size) {
 #----
 # do regression
 do_reg <- function(stock.symbol, trades, window.size) {
-    model <- "lr"
-    rows.number <- length(model) * length(stock.symbol) 
-    
-    pred.cols <- c("symbol", "date", "actual_final_price", "change", "model", 
-                   "predicted_price", "ME", "MAE", "RMSE", "MPE", "MAPE", "MASE")
-    pred.df <- data.frame(matrix(nrow=0, ncol=length(pred.cols)))
-    names(pred.df) <- pred.cols
-    
-    stock.df <- trades %>% filter(symbol == stock.symbol) %>% select(
-        "date", "final_price", "change")
-    
-    stock.ts <- ts(stock.df$final_price, frequency = 1)
-    
-    len <- length(stock.ts)
-    
-    valid.size <- 1
-    train.size <- window.size
-    
-    #rep(x, y): replicate x, y times
-    reg.me.list <- rep(0, len - train.size - valid.size + 1)
-    reg.mae.list <- rep(0, len - train.size - valid.size + 1) 
-    reg.rmse.list <- rep(0, len - train.size - valid.size + 1)
-    reg.mpe.list <- rep(0, len - train.size - valid.size + 1)
-    reg.mape.list <- rep(0, len - train.size - valid.size + 1)
-    reg.mase.list <- rep(0, len - train.size - valid.size + 1)
-    
-    for(j in 1 : (len - train.size - valid.size)) {
-        day.index <- j + train.size + valid.size
+    withProgress(message = 'Calculating', value = 0, {
+        model <- "lr"
+        rows.number <- length(model) * length(stock.symbol) 
         
-        train.ts <- window(stock.ts, start = j , end = j + train.size)
+        pred.cols <- c(
+            "symbol", "date", "actual_final_price", "change", "model", 
+            "predicted_price", "ME", "MAE", "RMSE", "MPE", "MAPE", "MASE"
+        )
+        pred.df <- data.frame(matrix(nrow=0, ncol=length(pred.cols)))
+        names(pred.df) <- pred.cols
         
-        valid.ts <- window(stock.ts, start = j + train.size + 1, 
-                           end = day.index)
+        stock.df <- trades %>% filter(symbol == stock.symbol) %>% select(
+            "date", "final_price", "change")
         
-        reg.fit <- tslm(train.ts ~ trend + I(trend^2))
+        stock.ts <- ts(stock.df$final_price, frequency = 1)
         
-        reg.pred <- forecast(reg.fit, h=valid.size, level=0)
+        len <- length(stock.ts)
         
-        reg.me.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"ME"]
-        reg.mae.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"MAE"]
-        reg.rmse.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"RMSE"]
-        reg.mpe.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"MPE"]
-        reg.mape.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"MAPE"]
-        reg.mase.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"MASE"]
+        valid.size <- 1
+        train.size <- window.size
         
-        if(j%%40==0){print(j)}
+        #rep(x, y): replicate x, y times
+        reg.me.list <- rep(0, len - train.size - valid.size + 1)
+        reg.mae.list <- rep(0, len - train.size - valid.size + 1) 
+        reg.rmse.list <- rep(0, len - train.size - valid.size + 1)
+        reg.mpe.list <- rep(0, len - train.size - valid.size + 1)
+        reg.mape.list <- rep(0, len - train.size - valid.size + 1)
+        reg.mase.list <- rep(0, len - train.size - valid.size + 1)
         
-        temp.pred.df <- data.frame(matrix(
-            nrow=1, ncol=length(pred.cols)))
+        for(j in 1 : (len - train.size - valid.size)) {
+            day.index <- j + train.size + valid.size
+            
+            train.ts <- window(stock.ts, start = j , end = j + train.size)
+            
+            valid.ts <- window(stock.ts, start = j + train.size + 1, 
+                               end = day.index)
+            
+            reg.fit <- tslm(train.ts ~ trend + I(trend^2))
+            
+            reg.pred <- forecast(reg.fit, h=valid.size, level=0)
+            
+            reg.me.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"ME"]
+            reg.mae.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"MAE"]
+            reg.rmse.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"RMSE"]
+            reg.mpe.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"MPE"]
+            reg.mape.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"MAPE"]
+            reg.mase.list[[j]] <- accuracy(reg.pred, valid.ts)[2,"MASE"]
+            
+            if(j%%40==0){print(j)}
+            
+            incProgress(
+                1/(len - train.size - valid.size), 
+                detail = paste(j, " th day")
+            )
+            
+            temp.pred.df <- data.frame(matrix(
+                nrow=1, ncol=length(pred.cols)))
+            
+            tdate <- stock.df[day.index,]$date
+            actual.price <- valid.ts[[1]]
+            pred.price <- reg.pred$mean[[1]]
+            
+            change <- stock.df[day.index,]$change
+            
+            temp.pred.df <- data.frame(
+                stock.symbol, tdate, actual.price, change, model, pred.price, 
+                reg.me.list[[j]], reg.mae.list[[j]], reg.rmse.list[[j]],
+                reg.mpe.list[[j]], reg.mape.list[[j]], reg.mase.list[[j]]
+            )
+            
+            colnames(temp.pred.df) <- pred.cols
+            
+            pred.df <- rbind(pred.df, temp.pred.df)
+        }
         
-        tdate <- stock.df[day.index,]$date
-        actual.price <- valid.ts[[1]]
-        pred.price <- reg.pred$mean[[1]]
+        p <- list("model" = model, "sym" = stock.symbol, 
+                  "mape" = mean(reg.mape.list), "rmse" = mean(reg.rmse.list))
+        print(p)
         
-        change <- stock.df[day.index,]$change
+        pred.df[,"date"] <- as.character(pred.df[,"date"])
         
-        temp.pred.df <- data.frame(
-            stock.symbol, tdate, actual.price, change, model, pred.price, 
-            reg.me.list[[j]], reg.mae.list[[j]], reg.rmse.list[[j]],
-            reg.mpe.list[[j]], reg.mape.list[[j]], reg.mase.list[[j]]
+        profit <- sign(
+            (
+                pred.df$predicted_price - 
+                shift_vector(pred.df$actual_final_price, 1)) * 
+                pred.df$change
         )
         
-        colnames(temp.pred.df) <- pred.cols
-        
-        pred.df <- rbind(pred.df, temp.pred.df)
-    }
-    
-    p <- list("model" = model, "sym" = stock.symbol, 
-              "mape" = mean(reg.mape.list), "rmse" = mean(reg.rmse.list))
-    print(p)
-    
-    pred.df[,"date"] <- as.character(pred.df[,"date"])
-    
-    profit <- sign(
-        (pred.df$predicted_price - shift_vector(pred.df$actual_final_price, 1))* 
-            pred.df$change
-    )
-    
-    result <- list(
-        "df" = pred.df,
-        
-        "error_parameters" = data.frame("mape" = p$mape, "rmse" = p$rmse),
-        
-        "predictions" = pred.df[,c("date", "actual_final_price", 
-                                   "predicted_price")],
-        
-        "residuals" = pred.df[,c("date", "ME")],
-        
-        "profits" = data.frame(
-            "profit" = c("Wrong", "Right"), 
-            "color" = c("red", "green"),
-            "sum" = c(
-                abs(sum(profit[profit == -1])),
-                sum(profit[profit != -1])
+        result <- list(
+            "df" = pred.df,
+            
+            "error_parameters" = data.frame("mape" = p$mape, "rmse" = p$rmse),
+            
+            "predictions" = pred.df[,c("date", "actual_final_price", 
+                                       "predicted_price")],
+            
+            "residuals" = pred.df[,c("date", "ME")],
+            
+            "profits" = data.frame(
+                "profit" = c("Wrong", "Right"), 
+                "color" = c("red", "green"),
+                "sum" = c(
+                    abs(sum(profit[profit == -1])),
+                    sum(profit[profit != -1])
+                )
+            ),
+            
+            "profit_percent" = round(
+                sum(profit[profit != -1]/length(profit)), 4
             )
-        ),
-        
-        "profit_percent" = round(sum(profit[profit != -1]/length(profit)), 4)
-        
-    )
+        )
+    })
     print("__________Finished__________")
     
     return (result)
